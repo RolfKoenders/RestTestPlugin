ab5bd6ca25d0c83872348cba4294ca9817956dfc
Rolf Koenders <post@madebyrolf.nl>
2024-07-02 22:21:52 +0200

    Initial commit

---
+++ src/main/java/nl/rolflab/resttest/RestTest.java
@@ -0,0 +1,17 @@
+package nl.rolflab.resttest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RestTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
