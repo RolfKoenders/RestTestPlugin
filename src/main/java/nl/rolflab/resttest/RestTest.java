da216cca308eb49295ccb47e0a362b530811762f
Rolf Koenders <post@madebyrolf.nl>
2024-07-03 21:12:08 +0200

    Refactored rest client and create basic endpoint handlers

--- src/main/java/nl/rolflab/resttest/RestTest.java
+++ src/main/java/nl/rolflab/resttest/RestTest.java
@@ -2,46 +2,54 @@ package nl.rolflab.resttest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import okhttp3.OkHttpClient;
+import com.google.gson.JsonObject;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public final class RestTest extends JavaPlugin {
 
     private String apiKey;
     private int port;
-    private MyApi api;
+    private Undertow server;
+    private RoutingHandler routingHandler;
+    private Gson gson;
 
     @Override
     public void onEnable() {
         loadConfig();
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .readTimeout(10, TimeUnit.SECONDS) // Set timeout (optional)
-                .build();
-
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        routingHandler = new RoutingHandler();
 
         String serverIp = getServer().getIp();
-        String baseUrl = "https://" + serverIp + ":" + port + "/";
+        if (serverIp.isEmpty()) {
+            serverIp = "0.0.0.0";
+        }
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(client)
+        server = Undertow.builder()
+                .addHttpListener(port, serverIp)
+                .setHandler(routingHandler)
                 .build();
 
-        // Create API interface
-        api = retrofit.create(MyApi.class);
+        server.start();
+        getLogger().info("REST server started on " + serverIp + ":" + port);
+
+        addRoute("GET", "/info", this::handleInfoRequest);
+        addRoute("GET", "/status", this::handleStatusRequest);
     }
 
-    // Load configuration from config.yml
+    @Override
+    public void onDisable() {
+        if (server != null) {
+            server.stop();
+            getLogger().info("REST server stopped.");
+        }
+    }
+
     private void loadConfig() {
         FileConfiguration config = getConfig();
         apiKey = config.getString("api-key");
@@ -49,23 +57,46 @@ public final class RestTest extends JavaPlugin {
         saveConfig();
     }
 
-    public interface MyApi {
-        @GET("/info")
-        retrofit2.Call<String> getInfo();
-    }
-
-    public String getPluginInfo() throws IOException {
-        retrofit2.Call<String> call = api.getInfo();
-        retrofit2.Response<String> response = call.execute();
-        if (response.isSuccessful()) {
-            return response.body();
-        } else {
-            throw new IOException("Failed to get info: " + response.errorBody().string());
+    public void addRoute(String method, String path, HttpHandler handler) {
+        switch (method.toUpperCase()) {
+            case "GET":
+                routingHandler.get(path, handler);
+                break;
+            case "POST":
+                routingHandler.post(path, handler);
+                break;
+            case "PUT":
+                routingHandler.put(path, handler);
+                break;
+            case "DELETE":
+                routingHandler.delete(path, handler);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported HTTP method: " + method);
         }
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public JsonObject getPluginInfo() {
+        JsonObject info = new JsonObject();
+        info.addProperty("plugin", "RestTest");
+        info.addProperty("version", "1.0.0");
+        return info;
+    }
+
+    public JsonObject getServerStatus() {
+        JsonObject status = new JsonObject();
+        status.addProperty("status", "server is running");
+        return status;
+    }
+
+    private void handleInfoRequest(io.undertow.server.HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+        JsonObject jsonResponse = getPluginInfo();
+        exchange.getResponseSender().send(gson.toJson(jsonResponse));
+    }
+
+    private void handleStatusRequest(io.undertow.server.HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+        exchange.getResponseSender().send(gson.toJson(this.getServerStatus()));
     }
 }
